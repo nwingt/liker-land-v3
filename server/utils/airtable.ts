dfbcee307456527b670892e31cd92064275141cc
Ng Wing Tat, David <i@ngwingt.at>
2025-11-18 23:43:51 +0800

    🐛 Use filtered search result

--- server/utils/airtable.ts
+++ server/utils/airtable.ts
@@ -87,6 +87,7 @@ export async function fetchAirtableCMSPublicationsBySearchTerm(
     `/${config.public.airtableCMSPublicationsTableId}`,
     {
       params: {
+        view: 'search',
         pageSize,
         filterByFormula,
         offset,
